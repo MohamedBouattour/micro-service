22d9c658db94aa5962415bc0b7f9d9c5e427c3d8
MohamedBouattour <mohamedbouattour123@gmail.com>
2023-06-01 01:14:52 +0100

    add eurika to node server

--- uml_designer/server/index.js
+++ uml_designer/server/index.js
@@ -5,18 +5,48 @@ const path = require("path");
 const fs = require("fs");
 const { getUmlFile } = require("./parser");
 const { spawn, spawnSync } = require("child_process");
+const Eureka = require("eureka-js-client").Eureka;
 
 const publicPath = path.join(__dirname, "public");
 app.use(express.static(path.join(publicPath, "uml_desginer")));
 app.use(express.json());
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(publicPath, "uml_desginer", "index.html"));
+});
+
+const eurekaConfig = {
+  eureka: {
+    host: 'eureka',
+    port: 8761,
+    servicePath: '/eureka/apps/',
+  },
+  instance: {
+    app: 'NodeApp',
+    instanceId: 'NodeApp',
+    hostName: 'localhost',
+    ipAddr: '127.0.0.1',
+    port: {
+      '$': 3000,
+      '@enabled': 'true',
+    },
+    vipAddress: 'NodeApp',
+    dataCenterInfo: {
+      '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+      name: 'MyOwn',
+    },
+  },
+};
+
+const eurekaClient = new Eureka(eurekaConfig);
+
+eurekaClient.start((error) => {
+  console.log(error || "Eureka registration complete");
 });
 
 app.post("/saveuml", (req, res) => {
   const data = getUmlFile(req.body.data);
-  fs.writeFile("../config/uml.json", JSON.stringify(data), err => {
+  fs.writeFile("../config/uml.json", JSON.stringify(data), (err) => {
     if (err) {
       console.error(err);
       return;
@@ -28,11 +58,11 @@ app.post("/saveuml", (req, res) => {
 
 app.get("/generate", (req, res) => {
   const child = spawn("yo", ["poc-ionic", "force"], { shell: true });
-  child.on("error", err => {
+  child.on("error", (err) => {
     console.error(`Error occurred while executing command: ${err}`);
     res.status(500).send({ message: "Error occurred while generating output" });
   });
-  child.on("close", code => {
+  child.on("close", (code) => {
     console.log(`Command exited with code ${code}`);
     res.status(200).send({ message: "generated successfully" });
   });
